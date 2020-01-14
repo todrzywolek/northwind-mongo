70735eeefd30cea1e5ff407d604d57a506bc2f31
Tomasz Szewczyk <tomasz.szewczyk02@gmail.com>
2020-01-14 01:40:22 +0100

    working repo import with types

--- mongo-import.sh
+++ mongo-import.sh
@@ -5,8 +5,10 @@ do
     filename="${filename%.*}"
     cat $f | tr -d \" > ${f}.tmp
     sed -i 's/NULL//g' ${f}.tmp
+    sed -i 's/null//g' ${f}.tmp
+    sed -i 's/,0x//g' ${f}.tmp
     rm $f
     mv ${f}.tmp $f
     echo $f
-    mongoimport -d northwind -c "$filename" --type csv --file "$f" --headerline
+    mongoimport -d northwind -c "$filename" --type csv --file "$f" --fieldFile "${f}.field" --columnsHaveTypes --ignoreBlanks
 done
